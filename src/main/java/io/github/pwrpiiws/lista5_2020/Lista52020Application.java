70d1bb6febcc291446edf415a4d0ed98d36887d7
Maciej Malecki <malecki.maciej@gmail.com>
2020-03-22 10:01:36 +0100

    Initial springboot

---
+++ src/main/java/io/github/pwrpiiws/lista5_2020/Lista52020Application.java
@@ -0,0 +1,13 @@
+package io.github.pwrpiiws.lista5_2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lista52020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lista52020Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/pwrpiiws/lista5_2020/Lista52020ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pwrpiiws.lista5_2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lista52020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
