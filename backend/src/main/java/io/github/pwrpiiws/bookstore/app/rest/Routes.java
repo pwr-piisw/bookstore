d2e1af4f55878c34ca12f7936b017e4533c9e766
Maciej Malecki <malecki.maciej@gmail.com>
2020-03-22 20:59:23 +0100

    Deploy angular app via WebFlux

--- backend/src/main/java/io/github/pwrpiiws/bookstore/app/rest/Routes.java
+++ backend/src/main/java/io/github/pwrpiiws/bookstore/app/rest/Routes.java
@@ -25,6 +25,7 @@ package io.github.pwrpiiws.bookstore.app.rest;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -35,13 +36,14 @@ public class Routes {
   public RouterFunction<ServerResponse> routerFunction(
       final BookHandler bookHandler, final ReviewHandler reviewHandler) {
     return RouterFunctions.route()
-        .GET("books", bookHandler::findAllBooks)
-        .GET("books/{bookId}", bookHandler::findBookById)
-        .POST("books", bookHandler::save)
-        .DELETE("books/{bookId}", bookHandler::delete)
-        .GET("reviews/{reviewId}", reviewHandler::findById)
-        .GET("books/{bookId}/reviews", reviewHandler::findAllReviewsForBook)
-        .POST("reviews", reviewHandler::save)
+        .GET("/books", bookHandler::findAllBooks)
+        .GET("/books/{bookId}", bookHandler::findBookById)
+        .POST("/books", bookHandler::save)
+        .DELETE("/books/{bookId}", bookHandler::delete)
+        .GET("/reviews/{reviewId}", reviewHandler::findById)
+        .GET("/books/{bookId}/reviews", reviewHandler::findAllReviewsForBook)
+        .POST("/reviews", reviewHandler::save)
+        .resources("/ui/**", new ClassPathResource("static/frontend/"))
         .build();
   }
 }
