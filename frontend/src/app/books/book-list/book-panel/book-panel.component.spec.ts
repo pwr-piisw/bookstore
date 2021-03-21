11cbbc320ed4e8a1cf5548730d842d47fd552b38
Maciej Malecki <malecki.maciej@gmail.com>
2021-03-21 20:56:00 +0100

    ~> angular 11

--- frontend/src/app/books/book-list/book-panel/book-panel.component.spec.ts
+++ frontend/src/app/books/book-list/book-panel/book-panel.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BookPanelComponent } from './book-panel.component';
 
@@ -6,7 +6,7 @@ describe('BookPanelComponent', () => {
   let component: BookPanelComponent;
   let fixture: ComponentFixture<BookPanelComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ BookPanelComponent ]
     })
